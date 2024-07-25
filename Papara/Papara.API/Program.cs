460bd23052606d88afe63035716f3494d31de6a6
kadiiravsaar <kadiiravsar@gmail.com>
2024-07-25 21:07:57 +0300

    Refactor Program.cs for improved API configurations

--- Papara/Papara.API/Program.cs
+++ Papara/Papara.API/Program.cs
@@ -1,15 +1,9 @@
-
 using Autofac.Extensions.DependencyInjection;
 using Autofac;
-
 using FluentValidation.AspNetCore;
-
 using Papara.API.Middleware;
-
 using Papara.Business.Validations;
-
 using Swashbuckle.AspNetCore.SwaggerUI;
-
 using System.Text.Json.Serialization;
 using Papara.API.Modules;
 using Microsoft.AspNetCore.Mvc;
@@ -23,28 +17,28 @@ namespace Papara.API
 		{
 			var builder = WebApplication.CreateBuilder(args);
 
-			// Add services to the container.
-
 			builder.Services.AddControllers(options => options.Filters.Add(new ValidateFilterAttribute()))
-				.AddFluentValidation(fv => { fv.RegisterValidatorsFromAssemblyContaining<CustomerRequestValidator>(); fv.DisableDataAnnotationsValidation = true; } )
+				.AddFluentValidation(fv => {
+					fv.RegisterValidatorsFromAssemblyContaining<CustomerRequestValidator>();
+					fv.DisableDataAnnotationsValidation = true; // Veri anotasyonlarýný devre dýþý býrak
+				})
 				.AddJsonOptions(options =>
-			{
-				options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles;
-				options.JsonSerializerOptions.WriteIndented = true;
-				options.JsonSerializerOptions.PropertyNamingPolicy = null;
-			});
+				{
+					options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles;
+					options.JsonSerializerOptions.WriteIndented = true;
+					options.JsonSerializerOptions.PropertyNamingPolicy = null;
+				});
 
 			builder.Services.Configure<ApiBehaviorOptions>(options =>
 			{
 				options.SuppressModelStateInvalidFilter = true;
-
 			});
-			// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
 			builder.Services.AddEndpointsApiExplorer();
 			builder.Services.AddSwaggerGen();
 
-
-			builder.Host.UseServiceProviderFactory(new AutofacServiceProviderFactory()); // Bu satýr, uygulamanýn varsayýlan hizmet saðlayýcý fabrikasýný Autofac ile deðiþtirmek için kullanýlýr.
+			
+			builder.Host.UseServiceProviderFactory(new AutofacServiceProviderFactory());
 			builder.Host.ConfigureContainer<ContainerBuilder>(containerBuilder =>
 			{
 				containerBuilder.RegisterModule(new AutofacModule(builder.Configuration));
@@ -52,7 +46,6 @@ namespace Papara.API
 
 			var app = builder.Build();
 
-			// Configure the HTTP request pipeline.
 			if (app.Environment.IsDevelopment())
 			{
 				app.UseSwagger();
@@ -65,8 +58,10 @@ namespace Papara.API
 			app.UseMiddleware<ErrorHandlerMiddleware>();
 			app.UseMiddleware<LoggerMiddleware>();
 			app.UseMiddleware<HeartbeatMiddleware>();
+
 			app.UseHttpsRedirection();
 			app.UseAuthorization();
+
 			app.MapControllers();
 
 			app.Run();
